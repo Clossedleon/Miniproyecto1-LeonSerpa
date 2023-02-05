23e494841a358445f42153a64c1e78b760b2934f
León Serpa <leon.serpa@correo.unimet.edu.ve>
2023-02-04 20:43:00 -0400

    Avance Skills

--- main.js
+++ main.js
@@ -18,10 +18,36 @@ BI.onclick = function () {
 
 let intervalo = null;
 
-const start = () => {
-    intervalo = setInterval(function (params) {
-        width = carrusel.scrollWidth
-        carruselItems.scrollLeft = carruselItems.scrollLeft + width;
-    }, 5000)
+const Barras = document.getElementById("barras")
+const SkillsMouse = document.getElementById("Skills")
+
+let skills = [
+    { label: "Python", value: 75 },
+    { label: "Java", value: 65 },
+    { label: "SQL", value: 70 },
+    { label: "Assembler", value: 40 },
+    { label: "HTML", value: 80 },
+    { label: "CSS", value: 60 },
+    { label: "Javascript", value: 30 },
+]
+let done = false
+
+function insertarBarras() {
+    if (!done) {
+        skills.forEach((item) => {
+
+        Barras.innerHTML += `
+        <div class="bloque">
+            <div class="barra">
+                <div class="progreso" id="${item.label}" style="height:${item.value}%"></div>
+            </div>
+            <h3>${item.label}</h3>
+        </div>
+        `
+        },
+        done = true
+        ) 
+    };
 }
 
+SkillsMouse.addEventListener("click", insertarBarras)
